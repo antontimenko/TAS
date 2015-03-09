201afa67f548210cc48ffd3cd3906a3c59f8c5cc
Anton Timenko <a.timenko-2020@kpi.ua>
2015-03-09 23:38:12 +0200

    Changed const string to char pointers in TSUtility.cpp

--- src/TSUtility.cpp
+++ src/TSUtility.cpp
@@ -3,31 +3,31 @@
 const char cLF = 0xA;
 const char cCR = 0xD;
 
-const string identifierChars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_@?$";
-const string numberChars = "1234567890";
-const string singleCharLexemChars = ",:[]+-";
-const string lexemDistributorChars = string(1, (char)0x9) + (char)0x20;
+const char identifierChars[] = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_@?$";
+const char numberChars[] = "1234567890";
+const char singleCharLexemChars[] = ",:[]()+-";
+const char lexemDistributorChars[] = {0x9, 0x20};
 const char commentChar = ';';
-const string quoteCompatibleChars = "\'\"";
+const char quoteCompatibleChars[] = "\'\"";
 
 bool isCharIdentifierCompatible(char ch)
 {
-    return identifierChars.find(ch) != string::npos;
+    return string(identifierChars).find(ch) != string::npos;
 }
 
 bool isCharNumberCompatible(char ch)
 {
-    return numberChars.find(ch) != string::npos;
+    return string(numberChars).find(ch) != string::npos;
 }
 
 bool isCharSingleCharacterLexemCompatible(char ch)
 {
-    return singleCharLexemChars.find(ch) != string::npos;
+    return string(singleCharLexemChars).find(ch) != string::npos;
 }
 
 bool isCharLexemDistributorCompatible(char ch)
 {
-    return lexemDistributorChars.find(ch) != string::npos;
+    return string(lexemDistributorChars).find(ch) != string::npos;
 }
 
 bool isCharCommentCompatible(char ch)
@@ -37,5 +37,5 @@ bool isCharCommentCompatible(char ch)
 
 bool isCharQuoteCompatible(char ch)
 {
-    return quoteCompatibleChars.find(ch) != string::npos;
+    return string(quoteCompatibleChars).find(ch) != string::npos;
 }
